dceaf537cdbea773c3f58029c2b665ccecbe8be7
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 15:38:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 
 template = """
-    Please generate 5 bullet points for a lesson plan on the following topic for a {grade} class:
+    Please generate 5 bullet points for a {difficulty} lesson plan on the following topic for a {grade} class:
     {lesson_description}
 
     YOUR LESSON PLAN BULLET POINTS:
 """
 
 prompt = PromptTemplate(
-    input_variables=["grade", "lesson_description"],
+    input_variables=["grade", "difficulty", "lesson_description"],
     template=template,
 )
 
@@ -31,13 +31,18 @@ st.header("Lesson Planner for Educators")
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
     option_grade = st.selectbox(
         'Which grade do you teach?',
         [str(x) for x in range(1, 13)])
-
+    
 with col2:
+    difficulty = st.selectbox(
+        'Difficulty level',
+        [str(x) for x in range(1, 5)])
+
+with col3:
     lesson_description = st.text_input("What do you want the lesson to be about?", "")
 
 if lesson_description:
@@ -49,7 +54,7 @@ if lesson_description:
 
     llm = load_LLM(openai_api_key=openai_api_key)
 
-    prompt_with_grade_and_description = prompt.format(grade=option_grade, lesson_description=lesson_description)
+    prompt_with_grade_and_description = prompt.format(grade=option_grade, lesson_description=lesson_description, difficulty=difficulty)
 
     lesson_plan = llm(prompt_with_grade_and_description)
 
