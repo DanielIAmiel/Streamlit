c2d56dd899bb8eb15246324c92b7ad7a817d7c91
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 14:39:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def load_LLM(openai_api_key):
 st.set_page_config(page_title="Lesson Planner for Educators", page_icon=":robot:")
 st.header("Lesson Planner for Educators")
 
-openai_api_key = OPENAI_API_KEY
+openai_api_key = apikey
 
 col1, col2 = st.columns(2)
 with col1:
