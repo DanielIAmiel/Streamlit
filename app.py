b924251a7a1b93d71ff69c1e67ef09e6ba90bf87
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 16:27:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ from langchain.llms import OpenAI
 # Import the API key from apikey.py
 import os
 
-# Set page configuration
+# Set up the page
 st.set_page_config(page_title="Lesson Planner for Educators", page_icon=":mortar_board:")
+st.title("Lesson Planner for Educators")
 
-# Set up the OpenAI API key
+# Get OpenAI API key
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-# Define the prompt for generating the lesson plan bullet points
+# Define the prompt template
 template = """
-    Please generate 5 bullet points for a lesson plan on the following topic for a {grade} class:
-    {lesson_description}
+Please generate 5 bullet points for a lesson plan on the following topic for a {grade} class:
+{lesson_description}
 
-    YOUR LESSON PLAN BULLET POINTS:
+YOUR LESSON PLAN BULLET POINTS:
 """
 
 prompt = PromptTemplate(
@@ -24,41 +25,84 @@ prompt = PromptTemplate(
     template=template,
 )
 
-# Define a function to load the OpenAI language model
+# Load OpenAI LLM
 def load_LLM(openai_api_key):
     llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
-# Define the layout of the page
-st.title("Lesson Planner for Educators")
-st.markdown("---")
-
-# Get user input for grade level and lesson topic
-col1, col2 = st.columns(2)
-with col1:
-    option_grade = st.selectbox(
-        'Which grade do you teach?',
-        ["Elementary School", "Middle School", "High School", "College", "Graduate School"],
-        index=3
-    )
-with col2:
-    lesson_description = st.text_input("What do you want the lesson to be about?", "")
-
-# Generate the lesson plan bullet points if the user has entered a lesson topic
-if lesson_description:
-    if not openai_api_key:
-        st.warning(
-            'Missing OpenAI API key',
-            icon="⚠️"
+# Define the form to get user input
+with st.form("lesson_planner"):
+    col1, col2 = st.columns([1, 2])
+    
+    with col1:
+        # Let user choose the grade level
+        option_grade = st.selectbox(
+            'Which grade do you teach?',
+            ["Elementary School", "Middle School", "High School", "College", "Graduate School"],
+            index=3  # Set default value to College
         )
+
+    with col2:
+        # Let user input the subject
+        subject = st.text_input("What is the subject of the lesson?")
+
+        # Let user input the lesson description
+        lesson_description = st.text_input("What do you want the lesson to be about?")
+
+        # Let user input the difficulty level
+        difficulty = st.selectbox(
+            'Difficulty level',
+            ["Beginner", "Intermediate", "Expert"],
+            index=1  # Set default value to Intermediate
+        )
+
+    # Let user submit the form
+    submit_button = st.form_submit_button(label="Generate Lesson Plan")
+
+# Generate the lesson plan
+if lesson_description and submit_button:
+    if not openai_api_key:
+        st.warning('Missing OpenAI API key')
         st.stop()
 
-    # Load the OpenAI language model and generate the lesson plan bullet points
     llm = load_LLM(openai_api_key=openai_api_key)
-    prompt_with_grade_and_description = prompt.format(grade=option_grade, lesson_description=lesson_description)
-    lesson_plan = llm(prompt_with_grade_and_description)
+    prompt_with_grade_subject_difficulty_and_description = prompt.format(
+        grade=option_grade, 
+        subject=subject,
+        difficulty=difficulty,
+        lesson_description=lesson_description
+    )
 
-    # Display the lesson plan bullet points
-    st.markdown("---")
-    st.markdown("### Your Lesson Plan Bullet Points:")
-    st.write("\n- " + "\n- ".join(lesson_plan.split("\n")))
+    lesson_plan = llm(prompt_with_grade_subject_difficulty_and_description)
+
+   
+    # Display the generated lesson plan
+st.markdown("### Your Lesson Plan Bullet Points:")
+st.write(lesson_plan)
+
+# Add a "Copy to Clipboard" button to copy the lesson plan to clipboard
+st.write("")
+copy_button = st.button("Copy to Clipboard")
+if copy_button:
+    st.experimental_set_query_params(text=lesson_plan)
+    st.success("Copied to Clipboard")
+
+# Add a "Tweet" button to share the lesson plan on Twitter
+tweet_button = st.button("Tweet")
+if tweet_button:
+    tweet_text = f"Check out this lesson plan I generated on {option_grade} {lesson_description} using #OpenAI and #Streamlit! {st.share_twitter(lesson_plan)}"
+    st.experimental_set_query_params(text=tweet_text)
+    st.success("Tweeted!")
+
+# Add a "Download" button to download the lesson plan as a text file
+download_button = st.button("Download as Text File")
+if download_button:
+    st.download_button(
+        label="Download Lesson Plan",
+        data=lesson_plan,
+        file_name=f"{option_grade}_{lesson_description}.txt",
+        mime="text/plain",
+    )
+
+
+
