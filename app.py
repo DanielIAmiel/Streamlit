9b0cfff1a22fde0862eefaffc52c0f3ca3ff7b9c
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 15:25:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 import streamlit as st
-import openai
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
 
 # Import the API key from apikey.py
+import os
+
 openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+template = """
+    Please generate 5 bullet points for a lesson plan on the following topic for a {grade} class:
+    {subject}: {lesson_description}
+
+    YOUR LESSON PLAN BULLET POINTS:
+"""
+
+prompt = PromptTemplate(
+    input_variables=["grade", "subject", "lesson_description"],
+    template=template,
+)
+
+def load_LLM(openai_api_key):
+    llm = OpenAI(temperature=0.5, engine="davinci", openai_api_key=openai_api_key)
+    return llm
 
 st.set_page_config(page_title="Lesson Planner for Educators", page_icon=":robot:")
 st.header("Lesson Planner for Educators")
 
-openai.api_key = openai_api_key
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
 
 col1, col2, col3 = st.columns(3)
 with col1:
@@ -16,35 +36,28 @@ with col1:
         [str(x) for x in range(1, 13)])
 
 with col2:
-    subject = st.text_input("Subject", "")
+    option_subject = st.selectbox(
+        'Which subject is this lesson for?',
+        ['Math', 'Science', 'English', 'Social Studies'])
 
 with col3:
     lesson_description = st.text_input("What do you want the lesson to be about?", "")
 
-if lesson_description and subject:
+if lesson_description:
     if not openai_api_key:
         st.warning(
             'Missing OpenAI API key',
             icon="⚠️")
         st.stop()
 
-    prompt = f"""
-    Please generate 5 bullet points for a lesson plan on the following topic for a {option_grade} {subject} class:
-    {lesson_description}
+    llm = load_LLM(openai_api_key=openai_api_key)
 
-    YOUR LESSON PLAN BULLET POINTS:
-    """
+    prompt_with_grade_subject_and_description = prompt.format(
+        grade=option_grade,
+        subject=option_subject,
+        lesson_description=lesson_description)
 
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=prompt,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    lesson_plan = response.choices[0].text.strip()
+    lesson_plan = llm(prompt_with_grade_subject_and_description)
 
     st.markdown("### Your Lesson Plan Bullet Points:")
     st.write(lesson_plan)
