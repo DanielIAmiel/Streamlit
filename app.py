380685711d8ee93c4513e0827e661be9c1fb809e
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 15:49:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ def load_LLM(openai_api_key):
     llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
-st.set_page_config(page_title="Lesson Planner for Educators", page_icon=":robot:")
-st.header("Lesson Planner for Educators")
+
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
