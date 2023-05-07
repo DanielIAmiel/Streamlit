23120795ef8f22f250f242558d6f0478b7ad1da2
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 15:17:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,20 @@ import os
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-
-
 template = """
-    Please generate 5 bullet points for a lesson plan on the following topic for a {grade} class:
+    Please generate 5 bullet points for a lesson plan on the following topic for a {grade} {subject} class:
     {lesson_description}
 
     YOUR LESSON PLAN BULLET POINTS:
 """
 
 prompt = PromptTemplate(
-    input_variables=["grade", "lesson_description"],
+    input_variables=["grade", "subject", "lesson_description"],
     template=template,
 )
 
 def load_LLM(openai_api_key):
-    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
+    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key, engine="text-davinci-002")
     return llm
 
 st.set_page_config(page_title="Lesson Planner for Educators", page_icon=":robot:")
@@ -30,28 +28,30 @@ st.header("Lesson Planner for Educators")
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
     option_grade = st.selectbox(
         'Which grade do you teach?',
         [str(x) for x in range(1, 13)])
 
 with col2:
+    subject = st.text_input("Subject", "")
+
+with col3:
     lesson_description = st.text_input("What do you want the lesson to be about?", "")
 
-if lesson_description:
+if lesson_description and subject:
     if not openai_api_key:
         st.warning(
-            'Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)',
+            'Missing OpenAI API key',
             icon="⚠️")
         st.stop()
 
     llm = load_LLM(openai_api_key=openai_api_key)
 
-    prompt_with_grade_and_description = prompt.format(grade=option_grade, lesson_description=lesson_description)
+    prompt_with_grade_subject_and_description = prompt.format(grade=option_grade, subject=subject, lesson_description=lesson_description)
 
-    lesson_plan = llm(prompt_with_grade_and_description)
+    lesson_plan = llm(prompt_with_grade_subject_and_description)
 
     st.markdown("### Your Lesson Plan Bullet Points:")
     st.write(lesson_plan)
