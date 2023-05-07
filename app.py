0121e3e3830822065f8da0caf736f945cc3eab0d
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 15:26:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,22 @@ import os
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
+
+
 template = """
     Please generate 5 bullet points for a lesson plan on the following topic for a {grade} class:
-    {subject}: {lesson_description}
+    {lesson_description}
 
     YOUR LESSON PLAN BULLET POINTS:
 """
 
 prompt = PromptTemplate(
-    input_variables=["grade", "subject", "lesson_description"],
+    input_variables=["grade", "lesson_description"],
     template=template,
 )
 
 def load_LLM(openai_api_key):
-    llm = OpenAI(temperature=0.5, engine="davinci", openai_api_key=openai_api_key)
+    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key, model=gpt-3.5-turbo)
     return llm
 
 st.set_page_config(page_title="Lesson Planner for Educators", page_icon=":robot:")
@@ -29,18 +31,13 @@ st.header("Lesson Planner for Educators")
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 with col1:
     option_grade = st.selectbox(
         'Which grade do you teach?',
         [str(x) for x in range(1, 13)])
 
 with col2:
-    option_subject = st.selectbox(
-        'Which subject is this lesson for?',
-        ['Math', 'Science', 'English', 'Social Studies'])
-
-with col3:
     lesson_description = st.text_input("What do you want the lesson to be about?", "")
 
 if lesson_description:
@@ -52,12 +49,9 @@ if lesson_description:
 
     llm = load_LLM(openai_api_key=openai_api_key)
 
-    prompt_with_grade_subject_and_description = prompt.format(
-        grade=option_grade,
-        subject=option_subject,
-        lesson_description=lesson_description)
+    prompt_with_grade_and_description = prompt.format(grade=option_grade, lesson_description=lesson_description)
 
-    lesson_plan = llm(prompt_with_grade_subject_and_description)
+    lesson_plan = llm(prompt_with_grade_and_description)
 
     st.markdown("### Your Lesson Plan Bullet Points:")
     st.write(lesson_plan)
