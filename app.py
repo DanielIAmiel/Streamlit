1bf06e4ea6d4b1410a30ee2bda4621f6cd5ee6f5
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 21:49:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+from io import StringIO
 
 # Set up the page
 st.set_page_config(page_title="Lesson Planner for Educators", page_icon=":mortar_board:")
@@ -49,38 +50,25 @@ if lesson_description and submit_button:
         st.stop()
 
     prompt_with_grade_subject_difficulty_and_description = template.format(
-        grade=option_grade, 
+        grade=option_grade,
         subject=subject,
         difficulty=difficulty,
         lesson_description=lesson_description
     )
 
-    # Create completion with streaming
+    # Prepare and send the request to the OpenAI API
     completions = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="text-davinci-002",
         prompt=prompt_with_grade_subject_difficulty_and_description,
-        n=1,
         max_tokens=1024,
-        temperature=0.7,
-        stop=None,  # You can set a stop token if needed
-        stream=True
+        n=1,
+        stop=None,
+        temperature=0.7
     )
 
-    # Display the generated lesson plan
-    st.markdown("### Your Lesson Plan:")
-    lesson_plan = ""
+    lesson_plan = StringIO()
 
-    from io import StringIO
-
-lesson_plan = StringIO()
-
-def handle_token(token):
-    lesson_plan.write(token.text)
-
-for chunk in completions.stream():
-    tokens = chunk.choices[0].tokens
-    for token in tokens:
-        handle_token(token)
+    # Process and display the tokens
+    for token in completions.choices[0].tokens:
+        lesson_plan.write(token.text)
         st.text_area("Your Lesson Plan:", value=lesson_plan.getvalue(), height=400, max_chars=None, key="lesson_plan")
-
-
