65d6401cd9da53eed3d80dd388d712bf324aaac1
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 21:37:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -72,13 +72,5 @@ if lesson_description and submit_button:
     )
     lesson_plan = llm(prompt_with_grade_subject_difficulty_and_description)
 
-    # Debugging lines
-    st.write(f"Type of lesson_plan: {type(lesson_plan)}")
-    st.write(f"Content of lesson_plan: {lesson_plan}")
-
-
-# Display the generated lesson plan
-st.markdown("### Your Lesson Plan:")
-st.text(lesson_plan)
-
+    
 
