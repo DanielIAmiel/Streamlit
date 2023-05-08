bc94c28a696c24bd0064276a5e9db65873225bdc
DanielIAmiel <129782091+DanielIAmiel@users.noreply.github.com>
2023-05-07 20:34:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,8 @@ if lesson_description and submit_button:
     lesson_plan = llm(prompt_with_grade_subject_difficulty_and_description)
 
     # Display the generated lesson plan
-    st.markdown("### Your Lesson Plan Bullet Points:")
-    lesson_plan = lesson_plan.strip().split("\n")
-    st.write("- " + "\n- ".join(lesson_plan))
+st.markdown("### Your Lesson Plan Bullet Points:")
+lesson_plan = lesson_plan.strip().split("\n")
+formatted_lesson_plan = "- " + "\n- ".join(lesson_plan)
+st.markdown(formatted_lesson_plan)
+
